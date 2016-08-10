e0c2d374852949954e04e935b25a474877f1fcfb
mmubasil <mb4740@bard.edu>
2016-08-10 09:36:02 -0400

    Update mubasilbot.js

--- mubasilbot.js
+++ mubasilbot.js
@@ -219,12 +219,12 @@ var rules = {
 };
 
 var default_responses = [
-  "Very interesting",
-  "I am not sure I understand you fully",
-  "What does that suggest to you?",
-  "Please continue",
-  "Go on",
-  "Do you feel strongly about discussing such things?",
+  "I'm an introvert, and I don't like talking to people. so, yeah, stop typing.",
+  "you should really improve your grammar.",
+  "*Rolls eyes*",
+  "*Sigh*",
+  "Blah blah blah. ",
+  "same.",
 ];
 
 function choice(lst) {
